98299b4d78eafa87053d95db853cbe44a856a1f1
Collins <Collins@MacBook-Pro-Admin.local>
2026-04-26 23:19:50 +0500

    use same domain newsletter api

--- src/admin/AdminDashboard.jsx
+++ src/admin/AdminDashboard.jsx
@@ -261,8 +261,7 @@ function AdminDashboard() {
     setSendingNewsletter(true);
 
     try {
-      const res = await fetch("https://maicinemafiles.pages.dev/api/send-newsletter", {
-        method: "POST",
+const res = await fetch("/api/send-newsletter", {        method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
